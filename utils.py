bd9cf43e6facc4582d3dc2d33bb133e19aaaab05
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2023-04-12 22:48:11 +0100

    Added support for classifiers

--- utils.py
+++ utils.py
@@ -25,20 +25,30 @@ def train_loop(dataloader, model: nn, loss_fn: callable, optimizer: Optimizer):
         optimizer.step()
 
 
-def test_loop(dataloader, model, loss_fn: callable, verbose: bool = False) -> float:
+def test_loop(dataloader, model, loss_fn: callable, classification: bool = False, verbose: bool = False) -> float:
     num_batches = len(dataloader)
     test_loss = 0
+    correct_labels = 0
+    tot_samples = 0
 
     with torch.no_grad():
         for X, y in dataloader:
             pred = model(X)
-            assert pred.shape == y.shape, 'Error - prediction/label shape mismatch!'
-            test_loss += loss_fn(pred, y).item()
+            assert classification or pred.shape == y.shape, 'Error - prediction/label shape mismatch!'
+
+            if classification:
+                tot_samples += y.shape[0]
+                correct_labels += (pred.max(1).indices == y).sum().item()
+            else:
+                test_loss += loss_fn(pred, y).item()
 
     test_loss /= num_batches
 
     if verbose:
-        print(f"Avg test loss: {test_loss:>8f} \n")
+        if classification:
+            print(f"Avg accuracy: {correct_labels / tot_samples:.3f} \n")
+        else:
+            print(f"Avg test loss: {test_loss:>8f} \n")
 
     return test_loss
 
