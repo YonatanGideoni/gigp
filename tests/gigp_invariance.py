be470ec11a436f69a0609581f6195f3ca69a183e
Yonatan Gideoni <yonatan.gideoni@gmail.com>
2023-04-13 22:19:43 +0100

    Updated test

--- tests/gigp_invariance.py
+++ tests/gigp_invariance.py
@@ -1,22 +1,23 @@
 import numpy as np
 import torch
 
+from architectures.baselines import NormalCNN
 from architectures.gigp import ImgGIGP
 from groups import SO2
 from utils import pixels2coords
 
 
 def test_invariance():
-    img = torch.tensor(np.random.rand(1, 1, 20, 20)).float()
+    img = torch.tensor(np.random.rand(1, 1, 28, 28)).float()
 
-    coords = pixels2coords(img)
+    coords = pixels2coords(h=28, w=28)
 
-    network = ImgGIGP(group=SO2(), coords=coords, in_dim=1)
+    model = ImgGIGP(group=SO2(), coords=coords, in_dim=1)
 
-    norm_out = network(img)
+    norm_out = model(img)
     rot_img = img.permute(0, 1, 3, 2).flip(3)
     for _ in range(3):
-        rot_out = network(rot_img)
+        rot_out = model(rot_img)
 
         assert torch.isclose(rot_out, norm_out).all(), "Error - GIGP isn't invariant!"
 
